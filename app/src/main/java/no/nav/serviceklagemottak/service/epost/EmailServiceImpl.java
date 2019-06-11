d1bbc99234c228661681bd129a9c9d426400fe5e
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-06-11 14:54:44 +0200

    MMA-3077 Prøver å sende fra annen adresse

--- app/src/main/java/no/nav/serviceklagemottak/service/epost/EmailServiceImpl.java
+++ app/src/main/java/no/nav/serviceklagemottak/service/epost/EmailServiceImpl.java
@@ -17,7 +17,7 @@ public class EmailServiceImpl {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        message.setFrom("srvserviceklagemot@nav.no");
+        message.setFrom("noreply-rekrutteringsbistand@nav.no");
         emailSender.send(message);
     }
 
