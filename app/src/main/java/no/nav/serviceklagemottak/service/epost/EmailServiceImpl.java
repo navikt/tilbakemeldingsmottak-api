ffa83bd9c840e9dd751310f23b74f99b5b6335c4
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-06-12 10:47:09 +0200

    MMA-3077 Disablet ssl, midlertidig kommentert ut mail-ting

--- app/src/main/java/no/nav/serviceklagemottak/service/epost/EmailServiceImpl.java
+++ app/src/main/java/no/nav/serviceklagemottak/service/epost/EmailServiceImpl.java
@@ -1,6 +1,5 @@
 package no.nav.serviceklagemottak.service.epost;
 
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +12,12 @@ public class EmailServiceImpl {
     private JavaMailSender emailSender;
 
     public void sendMail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        message.setFrom("srvpam-kandidatsok@preprod.local");
-        emailSender.send(message);
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(text);
+//        message.setFrom("");
+//        emailSender.send(message);
     }
 
 }
