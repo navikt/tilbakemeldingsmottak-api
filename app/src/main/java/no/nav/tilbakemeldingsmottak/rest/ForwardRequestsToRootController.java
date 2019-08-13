41c6bee1c46ad132a972104500f7ff67855ea94d
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-08-13 10:59:03 +0200

    MMA-3077 Gjort forwarder unprotected

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/ForwardRequestsToRootController.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/ForwardRequestsToRootController.java
@@ -1,5 +1,6 @@
 package no.nav.tilbakemeldingsmottak.rest;
 
+import no.nav.security.oidc.api.Unprotected;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Ugur Alpay Cenar, Visma Consulting.
  */
 @Controller
+@Unprotected
 public class ForwardRequestsToRootController {
 
     @RequestMapping(value = {"serviceklage/**"})
