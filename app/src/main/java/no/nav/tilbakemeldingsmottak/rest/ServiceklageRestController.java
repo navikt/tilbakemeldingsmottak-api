28fa5f7ae171bbcdd6bdee780a5892750606fa7b
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-09-10 16:24:02 +0200

    MMA-3077 Gjort hentServiceklage unprotected og endret hvor interceptor skal legges på

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/ServiceklageRestController.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/ServiceklageRestController.java
@@ -93,6 +93,7 @@ public class ServiceklageRestController {
 
     @Transactional
     @GetMapping(value = "/{journalpostId}")
+    @Unprotected
     public ResponseEntity<Serviceklage> hentServiceklage(@PathVariable String journalpostId) {
         try {
             Serviceklage response = serviceklageService.hentServiceklage(journalpostId);
