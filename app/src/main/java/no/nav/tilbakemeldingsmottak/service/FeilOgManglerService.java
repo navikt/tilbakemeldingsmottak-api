360afec7f5bd4e668eeabc15af215231aa53a3be
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-09-16 13:33:31 +0200

    MMA-3077 Prøvd å endre from-adresse i mail

--- app/src/main/java/no/nav/tilbakemeldingsmottak/service/FeilOgManglerService.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/service/FeilOgManglerService.java
@@ -33,7 +33,7 @@ public class FeilOgManglerService {
         message.setContent(createContent(request), "text/html; charset=UTF-8");
         MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
         helper.setTo("bjornar.hunshamar@trygdeetaten.no");
-        helper.setFrom("tilbakemeldinger-noreply@nav.no");
+        helper.setFrom("tilbakemeldinger-noreply@preprod.local");
         helper.setSubject("Feilmelding mottatt");
         emailService.sendMail(message);
     }
