332d2613cf55b97f618f00ad8c7c139ebd5cd072
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-09-24 14:04:33 +0200

    MMA-3077 Logger adresser i mail (test)

--- app/src/main/java/no/nav/tilbakemeldingsmottak/service/epost/EmailServiceImpl.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/service/epost/EmailServiceImpl.java
@@ -1,5 +1,6 @@
 package no.nav.tilbakemeldingsmottak.service.epost;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import javax.mail.internet.MimeMessage;
 
 @Profile({"nais", "itest"})
 @Component
+@Slf4j
 public class EmailServiceImpl extends AbstractEmailService{
 
     public EmailServiceImpl(JavaMailSender emailSender) {
@@ -17,6 +19,7 @@ public class EmailServiceImpl extends AbstractEmailService{
 
     @Override
     public void sendMail(MimeMessage message) throws MessagingException {
+        log.info("Sender mail til {} fra {}", message.getFrom(), message.getSender());
         emailSender.send(message);
     }
 }
