e8d6fca699bc396908c1cefbb20b04dce85385b1
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-10-21 12:15:47 +0200

    MMA-3077 Laget egen adresse-variabel i Vault for samisk kontakt

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/feilogmangler/service/FeilOgManglerService.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/feilogmangler/service/FeilOgManglerService.java
@@ -18,7 +18,7 @@ public class FeilOgManglerService {
 
     private AbstractEmailService emailService;
 
-    @Value("${email_to_address}")
+    @Value("${email_nav_support_address}")
     private String emailToAddress;
     @Value("${email_from_address}")
     private String emailFromAddress;
