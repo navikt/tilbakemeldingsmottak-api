a4741377ed0fcdd7dea91bbe8765c4da51e66cc7
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-10-24 10:29:54 +0200

    MMA-3077 Gjort det mulig å velge begge tidsrom ved bestilling av samtale

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/bestillingavsamtale/domain/Tidsrom.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/bestillingavsamtale/domain/Tidsrom.java
@@ -2,7 +2,8 @@ package no.nav.tilbakemeldingsmottak.rest.bestillingavsamtale.domain;
 
 public enum Tidsrom {
     FORMIDDAG ("8.00-10.00"),
-    ETTERMIDDAG ("13.30-15.30");
+    ETTERMIDDAG ("13.30-15.30"),
+    BEGGE (String.format("%s og %s", FORMIDDAG.text, ETTERMIDDAG.text));
 
     public final String text;
 
