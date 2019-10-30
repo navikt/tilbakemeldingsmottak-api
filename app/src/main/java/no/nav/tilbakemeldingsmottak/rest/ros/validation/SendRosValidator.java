ea2d881b2482b3b4554b790e035bf5bf91782e3b
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-10-30 13:34:06 +0100

    MMA-3077 Fjernet telefonnummer og gjort navn ikke-påkrevd i sendRos

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/ros/validation/SendRosValidator.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/ros/validation/SendRosValidator.java
@@ -8,8 +8,6 @@ import no.nav.tilbakemeldingsmottak.rest.ros.domain.SendRosRequest;
 public class SendRosValidator implements RequestValidator {
 
     public void validateRequest(SendRosRequest request) {
-        hasText(request.getNavn(), "navn");
-        hasText(request.getTelefonnummer(), "telefonnummer");
         isNotNull(request.getHvemRoses(), "hvemRoses");
         if(NAV_KONTOR.equals(request.getHvemRoses())) {
             hasText(request.getNavKontor(), "navKontor", " dersom hvemRoses=NAV_KONTOR");
