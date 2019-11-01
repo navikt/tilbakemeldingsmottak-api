584397842ace4b925735ce3224aacbe9866086ae
Halvard Kvaerna <halvard.hella.kverna@nav.no>
2019-11-01 15:05:12 +0100

    MMA-3750: Endret path til Redux store

--- web/src/main/web_src/src/index.js
+++ web/src/main/web_src/src/index.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/variables.less";
 import { ConnectedRouter } from "connected-react-router";
 import App from "./pages/App";
-import Store, { history } from "./store";
+import Store, { history } from "./store/store";
 
 export const store = Store();
 const target = document.getElementById("root");
