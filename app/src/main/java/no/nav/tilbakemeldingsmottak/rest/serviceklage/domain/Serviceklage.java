d7e3ca07800bad5f7dd89a370df9ced58e36c39e
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-11-04 12:58:33 +0100

    MMA-3077 Endret SQL-syntaks til Oracle og oppdatert db-alias i nais.yaml

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/domain/Serviceklage.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/domain/Serviceklage.java
@@ -26,8 +26,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -49,10 +47,7 @@ import java.time.LocalDateTime;
 public class Serviceklage {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "serviceklage_seq")
-    @GenericGenerator(name = "serviceklage_seq", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
-            @Parameter(name = "sequence_name", value = "serviceklage_serviceklage_id_seq")
-    })
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = SERVICEKLAGE_ID, nullable = false)
     private Long serviceklageId;
 
