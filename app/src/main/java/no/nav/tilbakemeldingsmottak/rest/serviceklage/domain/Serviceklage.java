98be3ea700ebc9edcf37a28290e9084785ce6a91
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-11-06 14:19:47 +0100

    MMA-3077 Oppdatert databasemodell

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/domain/Serviceklage.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/domain/Serviceklage.java
@@ -1,6 +1,10 @@
 package no.nav.tilbakemeldingsmottak.rest.serviceklage.domain;
 
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.AARSAK;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.AVSLUTTET_DATO;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.BEHANDLES_SOM_SERVICEKLAGE;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.BEHANDLES_SOM_SERVICEKLAGE_UTDYPNING;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.BESKRIVELSE;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.ENHETSNUMMER_BEHANDLENDE;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.ENHETSNUMMER_PAAKLAGET;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.FREMMET_DATO;
@@ -8,6 +12,7 @@ import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.Serviceklage
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.INNSENDER;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.JOURNALPOST_ID;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.KANAL;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.KANAL_UTDYPNING;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.KLAGEN_GJELDER_ID;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.KLAGETEKST;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.KLAGETYPER;
@@ -16,10 +21,11 @@ import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.Serviceklage
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.SCHEMA_VERSION;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.SERVICEKLAGE_ID;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.SVARMETODE;
-import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.SVAR_IKKE_NOEDVENDIG;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.SVARMETODE_UTDYPNING;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.TEMA;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.TEMA_UTDYPNING;
+import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.TILTAK;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.UTFALL;
-import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.VEILEDNING;
 import static no.nav.tilbakemeldingsmottak.rest.serviceklage.domain.ServiceklageConstants.YTELSE;
 
 import lombok.AllArgsConstructor;
@@ -69,6 +75,9 @@ public class Serviceklage {
     @Column(name = BEHANDLES_SOM_SERVICEKLAGE)
     private String behandlesSomServiceklage;
 
+    @Column(name = BEHANDLES_SOM_SERVICEKLAGE_UTDYPNING)
+    private String behandlesSomServiceklageUtdypning;
+
     @Column(name = FREMMET_DATO)
     private LocalDateTime fremmetDato;
 
@@ -77,6 +86,9 @@ public class Serviceklage {
 
     @Column(name = KANAL)
     private String kanal;
+
+    @Column(name = KANAL_UTDYPNING)
+    private String kanalUtdypning;
 
     @Column(name = ENHETSNUMMER_PAAKLAGET)
     private String enhetsnummerPaaklaget;
@@ -87,23 +99,35 @@ public class Serviceklage {
     @Column(name = GJELDER)
     private String gjelder;
 
+    @Column(name = BESKRIVELSE)
+    private String beskrivelse;
+
     @Column(name = YTELSE)
     private String ytelse;
 
     @Column(name = TEMA)
     private String tema;
 
-    @Column(name = VEILEDNING)
-    private String veiledning;
+    @Column(name = TEMA_UTDYPNING)
+    private String temaUtdypning;
 
     @Column(name = UTFALL)
     private String utfall;
 
+    @Column(name = AARSAK)
+    private String aarsak;
+
+    @Column(name = TILTAK)
+    private String tiltak;
+
     @Column(name = SVARMETODE)
     private String svarmetode;
 
-    @Column(name = SVAR_IKKE_NOEDVENDIG)
-    private String svarIkkeNoedvendig;
+    @Column(name = SVARMETODE_UTDYPNING)
+    private String svarmetodeUtdypning;
+
+    @Column(name = AVSLUTTET_DATO)
+    private String avsluttetDato;
 
     @Column(name = SCHEMA_VERSION)
     private Long schemaVersion;
