d6ad4eedf3f15c7ff50a9ca378828c248417ac49
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-11-11 13:26:21 +0100

    MMA-3077 Håndterer valideringsfeil i egen metode

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/common/handlers/ControllerAdvice.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/common/handlers/ControllerAdvice.java
@@ -3,6 +3,7 @@ package no.nav.tilbakemeldingsmottak.rest.common.handlers;
 import com.fasterxml.jackson.core.JsonParseException;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.tilbakemeldingsmottak.exceptions.InvalidIdentException;
+import no.nav.tilbakemeldingsmottak.exceptions.InvalidRequestException;
 import no.nav.tilbakemeldingsmottak.rest.common.domain.ErrorResponse;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
@@ -26,25 +27,34 @@ public class ControllerAdvice {
         return writer.getBuffer().toString();
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> technicalExceptionHandler(HttpServletRequest request, Exception ex) {
-        HttpStatus status = Optional.ofNullable(AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class))
+    private HttpStatus getHttpStatus(Exception ex) {
+        return Optional.ofNullable(AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class))
                 .map(ResponseStatus::code)
                 .orElse(HttpStatus.INTERNAL_SERVER_ERROR);
-
-        log.error("Feil i kall til " + request.getRequestURI() + ":\n" + getExceptionStacktrace(ex));
-        return ResponseEntity.status(status).body(ErrorResponse.builder().message(status.getReasonPhrase()).build());
     }
 
-    @ExceptionHandler(JsonParseException.class)
-    public ResponseEntity<ErrorResponse> jsonParseExceptionHandler(HttpServletRequest request, JsonParseException ex) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> technicalExceptionHandler(HttpServletRequest request, Exception ex) {
+        HttpStatus status = getHttpStatus(ex);
+        log.error("Feil i kall til " + request.getRequestURI() + ":\n" + getExceptionStacktrace(ex));
+        return ResponseEntity.status(status).body(ErrorResponse.builder()
+                .message(status.getReasonPhrase())
+                .build());
+    }
+
+    @ExceptionHandler(value = {JsonParseException.class, InvalidRequestException.class})
+    public ResponseEntity<ErrorResponse> validationExceptionHandler(HttpServletRequest request, JsonParseException ex) {
         log.warn("Feil i kall til " + request.getRequestURI() + ":\n" + getExceptionStacktrace(ex));
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder().message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
+                .message(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .build());
     }
 
     @ExceptionHandler(InvalidIdentException.class)
     public ResponseEntity<ErrorResponse> invalidIdentExceptionHandler(HttpServletRequest request, JsonParseException ex) {
         log.warn("Feil i kall til " + request.getRequestURI() + ":\n" + getExceptionStacktrace(ex));
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder().message(ex.getMessage()).build());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
+                .message(ex.getMessage())
+                .build());
     }
 }
