3a1dc96c9a23e785ad9ecf4e1bed982de05be45e
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-11-19 10:21:55 +0100

    MMA-3077 Setter consumer-id i MDC-context for innloggede saksbehandlere

--- app/src/main/java/no/nav/tilbakemeldingsmottak/interceptors/LoginRedirectInterceptor.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/interceptors/LoginRedirectInterceptor.java
@@ -3,7 +3,11 @@ package no.nav.tilbakemeldingsmottak.interceptors;
 import static no.nav.tilbakemeldingsmottak.config.Constants.LOGINSERVICE_ISSUER;
 import static no.nav.tilbakemeldingsmottak.config.Constants.REDIRECT_COOKIE;
 
+import com.nimbusds.jwt.SignedJWT;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
+import no.nav.security.oidc.context.TokenContext;
+import no.nav.tilbakemeldingsmottak.config.MDCConstants;
+import no.nav.tilbakemeldingsmottak.exceptions.OidcContextException;
 import no.nav.tilbakemeldingsmottak.util.CookieUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -26,10 +30,18 @@ public class LoginRedirectInterceptor extends AbstractInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 							 HttpServletResponse response, Object handler) throws Exception {
-		if (oidcRequestContextHolder.getOIDCValidationContext().getToken(LOGINSERVICE_ISSUER) == null) {
+		TokenContext token = oidcRequestContextHolder.getOIDCValidationContext().getToken(LOGINSERVICE_ISSUER);
+		if (token == null) {
 			response.addCookie(CookieUtils.createSessionCookie(REDIRECT_COOKIE, request.getRequestURI(), true));
 			response.sendRedirect(loginserviceUrl);
 			return false;
+		}
+		SignedJWT parsedConsumerToken = SignedJWT.parse(token.getIdToken());
+		String consumerId = parsedConsumerToken.getJWTClaimsSet().getSubject();
+		if (consumerId != null) {
+			addValueToMDC(consumerId, MDCConstants.MDC_CONSUMER_ID);
+		} else {
+			throw new OidcContextException("Kan ikke hente ut consumer-id fra token");
 		}
 		return true;
 	}
