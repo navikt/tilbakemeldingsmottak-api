de4641fc6b453721c7b7bdadefbe3ff12a7ea00c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-11-20 18:58:44 +0100

    MMA-3077 Endret litt på innhold i mail

--- app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/service/support/MailConstants.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/service/support/MailConstants.java
@@ -2,10 +2,10 @@ package no.nav.tilbakemeldingsmottak.rest.serviceklage.service.support;
 
 public class MailConstants {
     public static final String SUBJECT_KOMMUNAL_KLAGE = "Kommunal klage mottatt via serviceklageskjema på nav.no";
-    public static final String TEXT_KOMMUNAL_KLAGE = "Feilsendt klage ligger vedlagt.";
+    public static final String TEXT_KOMMUNAL_KLAGE = "En kommunal klage har blitt sendt inn via serviceklageskjema på nav.no. Denne har ikke blitt journalført eller lagret i serviceklagedatabasen. Feilsendt klage ligger vedlagt.";
 
     public static final String SUBJECT_JOURNALPOST_FEILET = "Automatisk journalføring av serviceklage feilet";
-    public static final String TEXT_JOURNALPOST_FEILET= "Manuell opprettelse av journalpost og oppgave kreves da automatisk mottak av serviceklage feilet. Se innholdet i klagen i vedlagt pdf.";
+    public static final String TEXT_JOURNALPOST_FEILET= "Manuell journalføring og opprettelse av oppgave kreves. Klagen ligger vedlagt.";
 
     public static final String SUBJECT_OPPGAVE_FEILET = "Automatisk opprettelse av oppgave feilet";
     public static final String TEXT_OPPGAVE_FEILET= "Manuell opprettelse av oppgave kreves for serviceklage med journalpostId=";
