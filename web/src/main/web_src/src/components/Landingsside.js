15ed86ff1d624eb7212394a655a7e1f7566dd77e
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-12-05 13:39:58 +0100

    MMA-3077 Videresender til gosys

--- web/src/main/web_src/src/components/Landingsside.js
+++ web/src/main/web_src/src/components/Landingsside.js
@@ -8,12 +8,20 @@ class Landingsside extends Component {
         super(props)
     }
 
+    componentDidMount() {
+        setTimeout(window.location = "https://gosys-nais-q1.nais.preprod.local/gosys/", 3000)
+    }
+
     render() {
 
         return (
             <div className="Landingsside">
                 <h1>Klassifisering fullført</h1>
                 <AlertStripe type="suksess">Serviceklagen er klassifisert og oppgaven har blitt lukket</AlertStripe>
+                <p>Du blir videresendt til <a href={"https://gosys-nais-q1.nais.preprod.local/gosys/"}>gosys</a> om tre sekunder.</p>
+
+                {}
+
             </div>
         )
 
