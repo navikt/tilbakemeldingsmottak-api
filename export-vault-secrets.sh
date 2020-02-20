3ab9b938d005020e8fd71831277a520464ff66a0
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2020-02-20 17:13:20 +0100

    MMA-3077 Fikset feil i eksportering av db-credentials

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -15,11 +15,11 @@ fi
 if test -f /var/run/secrets/nais.io/tilbakemeldingsmottak/username;
 then
     echo "Setting SPRING_DATASOURCE_USERNAME"
-    export SERVICEUSER_USERNAME=$(cat /var/run/secrets/nais.io/tilbakemeldingsmottak/username)
+    export SPRING_DATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/tilbakemeldingsmottak/username)
 fi
 
 if test -f /var/run/secrets/nais.io/tilbakemeldingsmottak/password;
 then
     echo "Setting SPRING_DATASOURCE_PASSWORD"
-    export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/tilbakemeldingsmottak/password)
+    export SPRING_DATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/tilbakemeldingsmottak/password)
 fi
