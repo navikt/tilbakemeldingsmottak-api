7b3927af07152e5799b6f025e59e489cbf141c83
nils-arne <50109859+nils-arne@users.noreply.github.com>
2021-08-26 14:33:44 +0200

    SD-1332 - Lagt inn logging av mail properties for feilsøking. Splittet saksbehandler restendepunkter fra innsendingsrestendepunkt for serviceklage. Lagt til endepunkt for sjekke om bruker er pålogget.

---
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/rest/serviceklage/AuthenticationCheckRestController.java
@@ -0,0 +1,45 @@
+package no.nav.tilbakemeldingsmottak.rest.serviceklage;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.security.token.support.core.api.Unprotected;
+import no.nav.tilbakemeldingsmottak.metrics.Metrics;
+import no.nav.tilbakemeldingsmottak.util.OidcUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static no.nav.tilbakemeldingsmottak.config.Constants.LOGINSERVICE_ISSUER;
+import static no.nav.tilbakemeldingsmottak.metrics.MetricLabels.DOK_REQUEST;
+import static no.nav.tilbakemeldingsmottak.metrics.MetricLabels.PROCESS_CODE;
+
+@Slf4j
+@Unprotected
+@RestController
+@RequestMapping("/rest/serviceklage")
+@RequiredArgsConstructor
+public class AuthenticationCheckRestController {
+
+    private final OidcUtils oidcUtils;
+
+    @GetMapping(value = "/autentisert")
+    @Metrics(value = DOK_REQUEST, extraTags = {PROCESS_CODE, "opprettServiceklage"}, percentiles = {0.5, 0.95}, histogram = true)
+    public ResponseEntity<Boolean>
+    isAuthenticated(@CookieValue(name = "isso-idtoken", required = false) String innlogget)
+        {
+
+        log.info("authenticationTest");
+        boolean loggetInn = oidcUtils.getSubject(innlogget) != null ? true : oidcUtils.getSubjectForIssuer(LOGINSERVICE_ISSUER).isPresent();
+        if (loggetInn) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(loggetInn);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(loggetInn);
+        }
+
+    }
+
+}
