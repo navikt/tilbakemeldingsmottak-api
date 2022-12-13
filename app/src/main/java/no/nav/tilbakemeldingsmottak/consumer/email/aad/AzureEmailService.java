5fbca8d903acb1808479510f40e66d23fdc837b5
nilsarne <nils-arne.ringstad@nav.no>
2022-12-13 10:05:01 +0100

    Har fått lagt inn srvtilbakemeldings i AAD. Fjernet midlertidig endret preprod config "JOURNALPOST_V1_URL": "https://dokarkiv-q1.nais.preprod.local/rest/journalpostapi/v1" for å teste feilet oppretting av journalpost slik at epost sendes.

--- app/src/main/java/no/nav/tilbakemeldingsmottak/consumer/email/aad/AzureEmailService.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/consumer/email/aad/AzureEmailService.java
@@ -12,10 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
 import java.util.List;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Objects;
 
 @Component
 public class AzureEmailService implements EmailService {
