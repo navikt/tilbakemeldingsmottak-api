5c1c382d6abe515a8e598e687d8e7bd9fe4188cd
Morten Stulen <morten.elias.stulen@nav.no>
2023-03-03 13:22:24 +0100

    Bruk postgres
    Og fiks på env variabler og logback

--- app/src/main/java/no/nav/tilbakemeldingsmottak/integration/sts/STSConfig.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/integration/sts/STSConfig.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
  * @author Ugur Alpay Cenar, Visma Consulting.
  */
 @Component
-@Profile({"nais", "local"})
+@Profile({"nais"})
 public class STSConfig {
 
 	@Value("${securityTokenService_url}")
