02e70dbe60a1698efd9f2088af866e3f7b928530
Morten Stulen <morten.elias.stulen@nav.no>
2023-03-08 09:37:19 +0100

    Legg til repository config

---
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/config/RepositoryConfigPostgres.java
@@ -0,0 +1,40 @@
+package no.nav.tilbakemeldingsmottak.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableConfigurationProperties(DataSourceProperties.class)
+@Profile("nais")
+public class RepositoryConfigPostgres {
+    private final DataSourceProperties dataSourceProperties;
+    private final Environment env;
+
+    public RepositoryConfigPostgres(DataSourceProperties dataSourceProperties, Environment env) {
+        this.dataSourceProperties = dataSourceProperties;
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        System.out.println("Environment: " + env.getProperty("DATABASE_HOST"));
+        System.out.println("Environment2: " + env.getProperty("NAIS_DATABASE_tilbakemeldingsmottak-api_tilbakemeldingsmottak-dev_HOST"));
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        return dataSource;
+    }
+}
