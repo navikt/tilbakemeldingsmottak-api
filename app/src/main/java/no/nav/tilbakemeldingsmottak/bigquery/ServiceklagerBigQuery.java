3a69b0bb66a403e6fe37e8395ebfd39400f2c376
Morten Stulen <morten.elias.stulen@nav.no>
2023-04-25 15:48:29 +0200

    Log errors

--- app/src/main/java/no/nav/tilbakemeldingsmottak/bigquery/ServiceklagerBigQuery.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/bigquery/ServiceklagerBigQuery.java
@@ -18,47 +18,54 @@ public class ServiceklagerBigQuery {
     private final BigQuery bigQueryClient;
 
     public void insertServiceklage(Serviceklage serviceklage) {
-        var map = new HashMap<String, Object>();
-        map.put("serviceklage_id", serviceklage.getServiceklageId());
-        map.put("journalpost_id", serviceklage.getJournalpostId());
-        map.put("opprettet_dato", serviceklage.getOpprettetDato());
-        map.put("klagen_gjelder_id", serviceklage.getKlagenGjelderId());
-        map.put("klagetyper", serviceklage.getKlagetyper());
-        map.put("gjelder_sosialhjelp", serviceklage.getGjelderSosialhjelp());
-        map.put("klagetekst", serviceklage.getKlagetekst());
-        map.put("behandles_som_serviceklage", serviceklage.getBehandlesSomServiceklage());
-        map.put("behandles_som_serviceklage_utdypning", serviceklage.getBehandlesSomServiceklageUtdypning());
-        map.put("fremmet_dato", serviceklage.getFremmetDato());
-        map.put("innsender", serviceklage.getInnsender());
-        map.put("kanal", serviceklage.getKanal());
-        map.put("kanal_utdypning", serviceklage.getKanalUtdypning());
-        map.put("enhetsnummer_paaklaget", serviceklage.getEnhetsnummerPaaklaget());
-        map.put("enhetsnummer_behandlende", serviceklage.getEnhetsnummerBehandlende());
-        map.put("gjelder", serviceklage.getGjelder());
-        map.put("beskrivelse", serviceklage.getBeskrivelse());
-        map.put("ytelse", serviceklage.getYtelse());
-        map.put("tema_utdypning", serviceklage.getTemaUtdypning());
-        map.put("veiledning", serviceklage.getVeiledning());
-        map.put("utfall", serviceklage.getUtfall());
-        map.put("aarsak", serviceklage.getAarsak());
-        map.put("tiltak", serviceklage.getTiltak());
-        map.put("svarmetode", serviceklage.getSvarmetode());
-        map.put("svarmetode_utdypning", serviceklage.getSvarmetodeUtdypning());
-        map.put("avsluttet_dato", serviceklage.getAvsluttetDato());
-        map.put("skjema_versjon", serviceklage.getSkjemaVersjon());
-        map.put("relatert", serviceklage.getRelatert());
-        map.put("klagetype_utdypning", serviceklage.getKlagetypeUtdypning());
-        map.put("innlogget", serviceklage.getInnlogget());
+        try {
+            var map = new HashMap<String, Object>();
+            map.put("serviceklage_id", serviceklage.getServiceklageId());
+            map.put("journalpost_id", serviceklage.getJournalpostId());
+            map.put("opprettet_dato", serviceklage.getOpprettetDato());
+            map.put("klagen_gjelder_id", serviceklage.getKlagenGjelderId());
+            map.put("klagetyper", serviceklage.getKlagetyper());
+            map.put("gjelder_sosialhjelp", serviceklage.getGjelderSosialhjelp());
+            map.put("klagetekst", serviceklage.getKlagetekst());
+            map.put("behandles_som_serviceklage", serviceklage.getBehandlesSomServiceklage());
+            map.put("behandles_som_serviceklage_utdypning", serviceklage.getBehandlesSomServiceklageUtdypning());
+            map.put("fremmet_dato", serviceklage.getFremmetDato());
+            map.put("innsender", serviceklage.getInnsender());
+            map.put("kanal", serviceklage.getKanal());
+            map.put("kanal_utdypning", serviceklage.getKanalUtdypning());
+            map.put("enhetsnummer_paaklaget", serviceklage.getEnhetsnummerPaaklaget());
+            map.put("enhetsnummer_behandlende", serviceklage.getEnhetsnummerBehandlende());
+            map.put("gjelder", serviceklage.getGjelder());
+            map.put("beskrivelse", serviceklage.getBeskrivelse());
+            map.put("ytelse", serviceklage.getYtelse());
+            map.put("tema_utdypning", serviceklage.getTemaUtdypning());
+            map.put("veiledning", serviceklage.getVeiledning());
+            map.put("utfall", serviceklage.getUtfall());
+            map.put("aarsak", serviceklage.getAarsak());
+            map.put("tiltak", serviceklage.getTiltak());
+            map.put("svarmetode", serviceklage.getSvarmetode());
+            map.put("svarmetode_utdypning", serviceklage.getSvarmetodeUtdypning());
+            map.put("avsluttet_dato", serviceklage.getAvsluttetDato());
+            map.put("skjema_versjon", serviceklage.getSkjemaVersjon());
+            map.put("relatert", serviceklage.getRelatert());
+            map.put("klagetype_utdypning", serviceklage.getKlagetypeUtdypning());
+            map.put("innlogget", serviceklage.getInnlogget());
 
-        log.debug(map.toString());
+            log.info(map.toString());
 
-        InsertAllRequest request = InsertAllRequest.newBuilder(TableId.of("serviceklager", "serviceklager")).addRow(map).build();
+            InsertAllRequest request = InsertAllRequest.newBuilder(TableId.of("serviceklager", "serviceklager")).addRow(map).build();
 
-        System.out.println("Inserting rows into table");
-        System.out.println(request.getRows());
+            log.info("Inserting rows into table");
+            log.info(request.getRows().toString());
 
-
-        bigQueryClient.insertAll(request);
-
+            var insertAllResponse = bigQueryClient.insertAll(request);
+            if (insertAllResponse.hasErrors()) {
+                insertAllResponse.getInsertErrors().forEach((k, v) -> log.error("Error inserting row: " + k + " " + v.toString()));
+                log.error("insertAllResponse has errors");
+            }
+            log.info(insertAllResponse.toString());
+        } catch (Exception e) {
+            log.error("Error inserting into BigQuery", e);
+        }
     }
 }
