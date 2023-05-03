cd13faf0bcce8665a4b7298df4ca0248181a4c6f
Morten Stulen <morten.elias.stulen@nav.no>
2023-05-03 13:56:33 +0200

    Eksluder UserDetailsServiceAutoConfiguration

--- app/src/main/java/no/nav/tilbakemeldingsmottak/Application.java
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/Application.java
@@ -2,12 +2,13 @@ package no.nav.tilbakemeldingsmottak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @ConfigurationPropertiesScan
 @EnableCaching
 public class Application {
