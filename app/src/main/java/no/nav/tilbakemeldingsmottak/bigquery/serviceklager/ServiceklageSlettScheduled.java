e72cfc4cec283ce9c681850d6742ef6fbaaa8096
Morten Stulen <morten.elias.stulen@nav.no>
2023-06-02 10:18:59 +0200

    Legg til schedule for sletting av Big Query serviceklager

---
+++ app/src/main/java/no/nav/tilbakemeldingsmottak/bigquery/serviceklager/ServiceklageSlettScheduled.java
@@ -0,0 +1,60 @@
+package no.nav.tilbakemeldingsmottak.bigquery.serviceklager;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Profile("nais")
+public class ServiceklageSlettScheduled {
+    private final BigQuery bigQueryClient;
+
+    @Value("${big_query_dataset}")
+    private String dataset;
+
+    @Value("${gcp_team_project_id}")
+    private String projectId;
+
+    @Value("${cron.slettBigQueryServiceKlagerEldreEnn}")
+    private String slettBigQueryServiceKlagerEldreEnn;
+
+    private String slettEldreEnnQuery(String datoFelt, String eventType) {
+        return String.format(
+                "DELETE FROM `%s.%s.%s` " +
+                        "WHERE TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), %s, DAY) >= %s " +
+                        "AND event_type=%s",
+                projectId, dataset, ServiceklagerBigQuery.TABLE_NAME,
+                datoFelt, slettBigQueryServiceKlagerEldreEnn,
+                eventType
+        );
+    }
+
+    @Scheduled(cron = "${cron.slettBigQueryServiceKlagerScheduled}")
+    public void slettServiceKlager() {
+        try {
+            log.info("Sletter serviceklager eldre enn {} dager", slettBigQueryServiceKlagerEldreEnn);
+
+            // Slett alle opprettede serviceklager som er eldre enn 1 uke
+            var slettOpprettedeServiceklager = slettEldreEnnQuery("opprettet_dato", ServiceklageEventTypeEnum.OPPRETT_SERVICEKLAGE.value);
+            var slettOpprettedeServiceklagerQueryConfig = QueryJobConfiguration.newBuilder(slettOpprettedeServiceklager).build();
+            bigQueryClient.query(slettOpprettedeServiceklagerQueryConfig);
+
+            // Slett alle klassifiserte serviceklager som er eldre enn 1 uke
+            var slettKlassifiserteServiceklager = slettEldreEnnQuery("avsluttet_dato", ServiceklageEventTypeEnum.KLASSIFISER_SERVICEKLAGE.value);
+            var slettKlassifiserteServiceklagerQueryConfig = QueryJobConfiguration.newBuilder(slettKlassifiserteServiceklager).build();
+            bigQueryClient.query(slettKlassifiserteServiceklagerQueryConfig);
+
+        } catch (Exception e) {
+            log.error("Kunne ikke slette Big Query serviceklager eldre enn {} dager", slettBigQueryServiceKlagerEldreEnn, e);
+        }
+
+
+    }
+}
