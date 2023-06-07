d9ba2bd5fd60bcbaa6c6c5a93c69d36a0fd2260e
Morten Stulen <morten.elias.stulen@nav.no>
2023-06-07 11:56:00 +0200

    Fjern ubrukte exceptions

--- app/src/main/java/no/nav/tilbakemeldingsmottak/exceptions/saf/MarshalHentDokumenterFraJoarkTechnicalException.java
+++
@@ -1,9 +0,0 @@
-package no.nav.tilbakemeldingsmottak.exceptions.saf;
-
-import no.nav.tilbakemeldingsmottak.exceptions.AbstractTilbakemeldingsmottakTechnicalException;
-
-public class MarshalHentDokumenterFraJoarkTechnicalException extends AbstractTilbakemeldingsmottakTechnicalException {
-    public MarshalHentDokumenterFraJoarkTechnicalException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
