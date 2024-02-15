1527ac1233b02f1a40670d4fcf14d543c7698b3b
nilsarne <nils-arne.ringstad@nav.no>
2024-02-15 08:39:28 +0100

    Revertert Metrics aspectet til å være dønn likt main, og flyttet consumer Metrics annotation for sending av epost til EmailService

--- app/src/main/kotlin/no/nav/tilbakemeldingsmottak/consumer/email/EmailService.kt
+++ app/src/main/kotlin/no/nav/tilbakemeldingsmottak/consumer/email/EmailService.kt
@@ -4,8 +4,20 @@ import no.nav.tilbakemeldingsmottak.metrics.MetricLabels
 import no.nav.tilbakemeldingsmottak.metrics.Metrics
 
 interface EmailService {
+    @Metrics(
+        value = MetricLabels.DOK_CONSUMER,
+        extraTags = [MetricLabels.PROCESS_CODE, "sendEpost"],
+        percentiles = [0.5, 0.95],
+        histogram = true
+    )
     fun sendSimpleMessage(mottaker: String, subject: String, content: String)
 
+    @Metrics(
+        value = MetricLabels.DOK_CONSUMER,
+        extraTags = [MetricLabels.PROCESS_CODE, "sendEpost"],
+        percentiles = [0.5, 0.95],
+        histogram = true
+    )
     fun sendSimpleMessage(mottakere: List<String>, subject: String, content: String)
 
     @Metrics(
